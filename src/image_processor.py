b967c345a2c8109ca024f173177db36f3658bcd4
schoko <justin.doerre@fu-berlin.de>
2024-11-30 23:54:29 +0100

    extract and show image from message, convert to gray

--- src/image_processor.py
+++ src/image_processor.py
@@ -3,22 +3,62 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard H:%s | W:%s", data.height, data.width)
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+
+bridge = CvBridge()
+
+def process_image(data):
+    rospy.loginfo(rospy.get_caller_id() + "H:%s | W:%s | Header:%s | Encoding: %s", data.height, data.width, data.header, data.encoding)
+    rospy.loginfo(rospy.get_caller_id() + "Data: step (row length in bytes) - %s", data.step)
+
+    try:
+        height, width = data.height, data.width        
+        # Konvertiere ROS-Bildnachricht in OpenCV-Bild
+        img = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+        
+        # Bild verarbeiten oder speichern
+        cv2.imwrite("result.jpg", img)
+        rospy.loginfo("Image saved successfully as result.jpg")
+        rospy.loginfo("Image Shape: %s", img.shape)
+        rospy.loginfo(img[0])
+
+        # Speichere graues Bild
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("result_gray.jpg", img_gray)
+        rospy.loginfo(img_gray.shape)
+        
+        # Schneide Halbkreis aus
+        mask = np.zeros((height, width), dtype=np.uint8)
+
+        center = (width // 2, height)
+        radius = int(width * 0.5)
+        cv2.circle(mask, center, radius, 255, -1)
+
+        mask_inverted = cv2.bitwise_not(mask)
+
+        output_image = img_gray.copy()
+        output_image[mask_inverted == 255] = [0]
+        
+        cv2.imwrite('result_masked.jpg', output_image)
+                      
+    except CvBridgeError as e:
+        rospy.logerr(f"Failed to convert image: {e}")  
     
-def listener():
+def image_processing_loop():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('image_processor', anonymous=True)
 
-    rospy.Subscriber("sensors/hella/image", Image, callback)
+    rospy.Subscriber("sensors/hella/image", Image, process_image)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
   
 if __name__ == '__main__':
-    listener()
+    image_processing_loop()
